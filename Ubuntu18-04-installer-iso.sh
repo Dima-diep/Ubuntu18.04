288b911c1c9eb3033aa03a8d1adb0a3303410f23
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-07-07 12:07:05 +0400

    Ubuntu-18-04-installer-iso.sh

--- Ubuntu18-04-installer-iso.sh
+++ Ubuntu18-04-installer-iso.sh
@@ -7,21 +7,6 @@ pkg upgrade -y
 pkg install curl -y
 pkg install proot -y 
 pkg install tar -y
+cd ~/
 curl https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Ubuntu/ubuntu.sh | bash
-rm -rf ubuntu18-04-installer-iso.sh
-chmod +x *
-./start-ubuntu.sh
-apt update -y
-apt upgrade -y
-apt install sudo -y
-sudo -i
-apt install git -y
-apt install python -y
-apt install python2 -y
-apt install python3 -y
-apt install curl -y
-apt install wget -y
-apt install pip -y
-apt install python-pip -y
-apt install python3-pip -y
-echo "Hello, dear User. You've installed Ubuntu 18.04 on your smartphone! You can use root commands even without real root!"
+rm -rf Ubuntu18.04
